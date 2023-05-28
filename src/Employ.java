3d4a4782785b55209411a2b8e8b932a2de12159e
Mohd Faijan <faiz3ica@gmail.com>
2023-05-28 14:32:11 +0530

    Added Regex

--- src/Employ.java
+++ src/Employ.java
@@ -7,19 +7,24 @@ import java.util.regex.Pattern;
 public class Employ {
     private Long empId;
     private String empName;
-    private static Set<Long> Ids=new HashSet<>();
+    private static Set<Long> Ids = new HashSet<>();
+
     public Employ() {
 
     }
 
     public Employ(String empName) {
-        if (empName.length() >= 3){
-            generateEmpId();
-            this.empName = empName;
-            System.out.println("EmpID:"+this.empId);
-        }
-        else
+        if (empName.length() >= 3) {
+            if (Pattern.matches("[a-zA-Z-\\s]+", empName)) {
+                generateEmpId();
+                this.empName = empName;
+                System.out.println("EmpID:" + this.empId);
+            } else {
+                System.out.println("Name sould not be contain digit or special character");
+            }
+        } else{
             System.out.println("Name should be 3 or grater than 3 character");
+        }
     }
 
     public Long getEmpId() {
@@ -32,14 +37,19 @@ public class Employ {
 
     public void setEmpName(String empName) {
         if (empName.length() >= 3) {
-            this.empName = empName;
-            System.out.println("EmpID:"+this.empId);
+            if (Pattern.matches("[a-zA-Z-\\s]+", empName)) {
+                generateEmpId();
+                this.empName = empName;
+                System.out.println("EmpID:" + this.empId);
+            } else {
+                System.out.println("Name sould not be contain digit or special character");
+            }
         } else
             System.out.println("Name should be 3 or greater than 3 Character");
     }
 
     private void generateEmpId() {
-        this.empId=Long.valueOf(Ids.size()+1);
+        this.empId = Long.valueOf(Ids.size() + 1);
         Ids.add(this.empId);
     }
 }
